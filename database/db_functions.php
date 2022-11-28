8eb4108f0fcb3abd7890d426a1ebbb45a88547c4
S-furi <stefano.furi@studio.unibo.it>
2022-11-28 23:47:01 +0100

    Implemented DB handles for login.

---
+++ database/db_functions.php
@@ -0,0 +1,75 @@
+<?php
+// http://paololatella.blogspot.com/2017/10/lezione-nov-2017-script-sicuro-per-il.html
+function sec_session_start() {
+  $session_name = "sec_session_id";
+  $secure = false; // not https
+  $httponly = true; // js can't access to sess-id
+  ini_set("session.use_only_cookies", 1); // force session to use cookies only
+  $cookie_params = session_get_cookie_params();
+  session_set_cookie_params($cookie_params['lifetime'], $cookie_params['path'], $cookie_params['domain'], $secure, $httponly);
+  session_name($session_name);
+  session_start();
+  session_regenerate_id(); // regenerate the session and delete the one created before
+}
+
+/**
+ * Checks wether a provided email and password are inside
+ * the database. 
+ */
+function login($email, $password, $mysqli) {
+  $query = "SELECT usrId, password, username FROM users WHERE email=? LIMIT 1";
+  if ($stmt = $mysqli->prepare($query)) {
+    $stmt->bind_param('s', $email);
+    $stmt->execute();
+    $stmt->store_result();
+
+    if ($stmt->num_rows != 1) {
+      return false;
+    }
+
+    $data = $stmt->get_result()->fetch_all(MYSQLI_ASSOC)[0];
+    
+    $username = $data['username'];
+    $user_id = $data['usrId'];
+    $db_password = $data['password'];
+
+    if (checkBrute($user_id, $mysqli) || password_verify($password, $db_password) ) {
+      return false;
+    }
+    // correct password inserted
+
+    // user broser should be inserted crypted in 
+    // $_SESSION['login_string'] with psw;
+
+    // $user_browser = $_SERVER['HTTP_USER_AGENT'];
+
+    // trying to avoid XSS attacks using htmlentities()
+    // used for replacing with HTML escape sequences
+    // insed of symbols like <, >, / or others simple ways
+    // to inject malicious code inside forms.
+    $_SESSION['user_id'] = htmlentities($user_id); 
+    $_SESSION['username'] = htmlentities($username); 
+    return true;
+  }
+}
+
+/**
+ * Checks wether the account is being brute forced 
+ * by checking if there are more than 5 failed
+ * login attempts in the last two hours.
+ */
+function checkBrute($usr_id, $mysqli) : bool {
+  $now = time();
+  $valid_attempts = $now - (2 * 60 * 60);
+  if ($stmt = $mysqli->prepare("SELECT time FROM login_attempts WHERE usrId=? ABD tune > '$valid_attempts'")) {
+    $stmt->bind_param("i", $usr_id);
+    $stmt->execute();
+    $stmt->store_result();
+    if ($stmt->num_rows > 5) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+?>
