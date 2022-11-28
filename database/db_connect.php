5af02a61ed0fb17721bae82392e9baa9458cd5fa
S-furi <stefano.furi@studio.unibo.it>
2022-11-28 18:13:38 +0100

    DB connection handler

---
+++ database/db_connect.php
@@ -0,0 +1,9 @@
+<?php
+define("HOST", "localhost");
+define("USER", "sec_user");
+define("PASSWORD", "eKcGZr59zAa2BEWU");
+define("DATABASE", "brogram");
+
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
+
+?>
