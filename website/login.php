b90125b5d81a0796d578a2615b96560db5f6d632
S-furi <stefano.furi@studio.unibo.it>
2022-11-29 14:58:07 +0100

    Implemented database check.

--- website/login.php
+++ website/login.php
@@ -1,7 +1,19 @@
 <?php
+require_once('../database/db_connect.php');
+require_once('../database/db_functions.php');
+
+sec_session_start();
 
 $templateParams["Titolo"] = "Brogram - Login";
 $templateParams["nome"] = "login-form.php";
+
+if (isset($_POST['email'], $_POST['password'])) {
+  if (login($_POST['email'], $_POST['password'], $mysqli)) {
+    echo "login avvennuto con successo";
+  } else {
+    echo "login fallito!";
+  }
+}
 
 require("template/base.php");
 
