7c7453c4d7aab4fa6846ffbeafc50715d56907c2
S-furi <stefano.furi@studio.unibo.it>
2022-11-29 12:45:13 +0100

    Inserting a new user in DB when signing up

--- website/signup.php
+++ website/signup.php
@@ -1,7 +1,22 @@
 <?php
+require_once('../database/db_connect.php');
+require_once('../database/db_functions.php');
 
 $templateParams["Titolo"] = "Brogram - Sign Up";
 $templateParams["nome"] = "signup-form.php";
+
+if (isset($_POST['mail'], $_POST['password'], $_POST['username'])) {
+  $email = $_POST['mail'];
+  $password = $_POST['password'];
+  $username = $_POST['username'];
+
+  if (insertNewUser($email, $password, $username, $mysqli)) {
+    echo "utente inserito";
+    // REDIRECT SULLA HOME
+  } else {
+    echo "errore durante l'inserimento";
+  }
+}
 
 require("template/base.php");
 
