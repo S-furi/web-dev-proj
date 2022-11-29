49b6b7b3919a969d14f9e1291d5407a771e743d0
S-furi <stefano.furi@studio.unibo.it>
2022-11-29 12:45:52 +0100

    Reorganized references of fileds name.

--- website/template/signup-form.php
+++ website/template/signup-form.php
@@ -1,20 +1,20 @@
-        <form action="#" method="POST">
+        <form action="#" method="post" name="signup_form">
             <h2>Sign up</h2>
             <ul>
+                <!-- <li> -->
+                    <!-- <label for="profilepic">Profile Picture:</label><input type="file" id="profilepic"  me="profilepic" /> -->
+                <!-- </li> -->
                 <li>
-                    <label for="profilepic">Profile Picture:</label><input type="file" id="profilepic"  me="profilepic" />
+                    <label for="mail">Email:</label><input type="text" id="mail" name="mail"/>
                 </li>
                 <li>
-                    <label for="mail">Email:</label><input type="text" id="mail"  me="mail" />
+                    <label for="username">Username:</label><input type="text" id="username" name="username" />
                 </li>
                 <li>
-                    <label for="username">Username:</label><input type="text" id="username"  me="username" />
+                    <label for="password">Password:</label><input type="password" id="password" name="password" />
                 </li>
                 <li>
-                    <label for="password">Password:</label><input type="password" id="passwo " name="password" />
-                </li>
-                <li>
-                    <input type="submit" name="submit" value="Invia" />
+                    <input type="button" name="sub_register" value="Invia" onclick="form.submit()" />
                 </li>
             </ul>
         </form>
