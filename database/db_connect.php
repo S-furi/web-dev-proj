105ffedfd5c7404245c5ee4d5710e5765bb1f21d
S-furi <stefano.furi@studio.unibo.it>
2022-11-29 12:44:04 +0100

    Now displaying errors when connecting to DB.

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,9 +1,12 @@
 <?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 define("HOST", "localhost");
 define("USER", "sec_user");
 define("PASSWORD", "eKcGZr59zAa2BEWU");
 define("DATABASE", "brogram");
 
 $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
-
 ?>
