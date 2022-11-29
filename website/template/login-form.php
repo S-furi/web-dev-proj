9cf9d1371a3432639a244c91bfc0a51853c19ffe
S-furi <stefano.furi@studio.unibo.it>
2022-11-29 14:58:19 +0100

    Now attributes names match db names.

--- website/template/login-form.php
+++ website/template/login-form.php
@@ -2,13 +2,13 @@
     <h2>Login</h2>
     <ul>
         <li>
-            <label for="username">Username:</label><input type="text" id="username"  me="username" />
+            <label for="email">Email:</label><input type="text" id="email" name="email" />
         </li>
         <li>
-            <label for="password">Password:</label><input type="password" id="passwo " name="password" />
+            <label for="password">Password:</label><input type="password" id="password" name="password" />
         </li>
         <li>
-            <input type="submit" name="submit" value="Invia" />
+            <input type="submit" name="sub_login" value="Invia" onclick="this.form.submit()" />
         </li>
     </ul>
 </form>
