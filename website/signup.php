3c16dcb36f622977cf799a1e7d51e59c9cf444a0
S-furi <stefano.furi@studio.unibo.it>
2022-11-30 20:54:02 +0100

    Registering first and lastname.

--- website/signup.php
+++ website/signup.php
@@ -7,12 +7,14 @@ sec_session_start();
 $templateParams["Titolo"] = "Brogram - Sign Up";
 $templateParams["nome"] = "signup-form.php";
 
-if (isset($_POST['mail'], $_POST['password'], $_POST['username'])) {
+if (isset($_POST['mail'], $_POST['password'], $_POST['username'], $_POST['first_name'], $_POST['last_name'])) {
   $email = $_POST['mail'];
   $password = $_POST['password'];
   $username = $_POST['username'];
+  $first_name = $_POST['first_name'];
+  $last_name = $_POST['last_name'];
 
-  if (insertNewUser($email,$username, $password, $mysqli)) {
+  if (insertNewUser($email,$username, $password, $first_name, $last_name, $mysqli)) {
     echo "utente inserito";
     // REDIRECT SULLA HOME
   } else {
