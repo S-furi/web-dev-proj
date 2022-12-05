fe255426456106efc1ce5d4734e3b55f28ae5619
Michele Montesi <michele.montesi3@studio.unibo.it>
2022-12-05 17:45:57 +0100

    added cols in base main

--- website/template/home-template.php
+++ website/template/home-template.php
@@ -1,26 +1,3 @@
-<div class="wrapper">
-
-    <div class="left-col">
-        <div class="col-items">
-            <img src="img/home.PNG" class="icon" alt="">
-        </div> 
-        <div class="col-items">
-            <img src="img/messenger.PNG" class="icon" alt="">
-        </div> 
-        <div class="col-items">
-            <img src="img/add.PNG" class="icon" alt="">
-        </div> 
-        <div class="col-items">
-            <img src="img/explore.PNG" class="icon" alt="">
-        </div> 
-        <div class="col-items">
-            <img src="img/like.PNG" class="icon" alt="">
-        </div> 
-        <div class="col-items">
-            <div class="icon user-profile"></div>
-        </div> 
-    </div>
-
     <div class="center-col">
         <div class="post">
             <div class="info">
@@ -45,11 +22,6 @@
                 <p class="description"><span>username </span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur tenetur veritatis placeat, molestiae impedit aut provident eum quo natus molestias?</p>
                 <p class="post-time">2 minutes ago</p>
             </div>
-            <!-- <div class="comment-wrapper">
-                <img src="img/smile.PNG" class="icon" alt="">
-                <input type="text" class="comment-box" placeholder="Add a comment">
-                <button class="comment-btn">post</button>
-            </div> -->
         </div>
         <div class="post">
             <div class="info">
@@ -74,76 +46,5 @@
                 <p class="description"><span>username </span> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur tenetur veritatis placeat, molestiae impedit aut provident eum quo natus molestias?</p>
                 <p class="post-time">2 minutes ago</p>
             </div>
-            <!-- <div class="comment-wrapper">
-                <img src="img/smile.PNG" class="icon" alt="">
-                <input type="text" class="comment-box" placeholder="Add a comment">
-                <button class="comment-btn">post</button>
-            </div> -->
         </div>
     </div>
-
-    <div class="right-col">
-        <div class="profile-card">
-            <div class="profile-pic">
-                <img src="img/profile-pic.png" alt="">
-            </div>
-            <div>
-                <p class="username">username</p>
-                <p class="sub-text">username</p>
-                <button class="action-btn">switch</button>
-            </div>
-        </div>
-        <p class="suggestion-text">Suggestions for you</p>
-        <div class="profile-card">
-            <div class="profile-pic">
-                <img src="img/cover 9.png" alt="">
-            </div>
-            <div>
-                <p class="username">username</p>
-                <p class="sub-text">followed by user</p>
-                <button class="action-btn">follow</button>
-            </div>
-        </div>
-        <div class="profile-card">
-            <div class="profile-pic">
-                <img src="img/cover 10.png" alt="">
-            </div>
-            <div>
-                <p class="username">username</p>
-                <p class="sub-text">followed by user</p>
-                <button class="action-btn">follow</button>
-            </div>
-        </div>
-        <div class="profile-card">
-            <div class="profile-pic">
-                <img src="img/cover 11.png" alt="">
-            </div>
-            <div>
-                <p class="username">username</p>
-                <p class="sub-text">followed by user</p>
-                <button class="action-btn">follow</button>
-            </div>
-        </div>
-        <div class="profile-card">
-            <div class="profile-pic">
-                <img src="img/cover 12.png" alt="">
-            </div>
-            <div>
-                <p class="username">username</p>
-                <p class="sub-text">followed by user</p>
-                <button class="action-btn">follow</button>
-            </div>
-        </div>
-        <div class="profile-card">
-            <div class="profile-pic">
-                <img src="img/cover 13.png" alt="">
-            </div>
-            <div>
-                <p class="username">username</p>
-                <p class="sub-text">followed by user</p>
-                <button class="action-btn">follow</button>
-            </div>
-        </div>
-    </div>
-    
-</div>
--- website/template/newBase.php
+++ website/template/newBase.php
@@ -10,16 +10,103 @@
 <body>
     <nav class="navbar">
         <div class="nav-wrapper">
-            <!-- <img src="../img/logo.PNG" class="brand-img" alt=""> -->
             <h1 class="brand-img">Brogram</h1>
             <input type="text" class="search-box" placeholder="search" id="search-field">
             <button type="button" onclick="trialButtonClick()" class="search-btn" id="search-btn">Search</button>
         </div>
     </nav>
     <section class="main">
-    <?php
-        require($templateParams["nome"]);
-    ?>
+    <div class="wrapper">
+        <div class="left-col">
+            <div class="col-items">
+                <a href="index.php"><img src="img/home.PNG" class="icon" alt=""></a>
+            </div> 
+            <div class="col-items">
+                <img src="img/messenger.PNG" class="icon" alt="">
+            </div> 
+            <div class="col-items">
+                <img src="img/add.PNG" class="icon" alt="">
+            </div> 
+            <div class="col-items">
+                <img src="img/explore.PNG" class="icon" alt="">
+            </div> 
+            <div class="col-items">
+                <img src="img/like.PNG" class="icon" alt="">
+            </div> 
+            <div class="col-items">
+                <div class="icon user-profile"></div>
+            </div> 
+        </div>
+
+        <?php
+            require($templateParams["nome"]);
+        ?> 
+        
+        <div class="right-col">
+            <div class="profile-card">
+                <div class="profile-pic">
+                    <img src="img/profile-pic.png" alt="">
+                </div>
+                <div>
+                    <p class="username">username</p>
+                    <p class="sub-text">username</p>
+                    <button class="action-btn">switch</button>
+                </div>
+            </div>
+            <p class="suggestion-text">Suggestions for you</p>
+            <div class="profile-card">
+                <div class="profile-pic">
+                    <img src="img/cover 9.png" alt="">
+                </div>
+                <div>
+                    <p class="username">username</p>
+                    <p class="sub-text">followed by user</p>
+                    <button class="action-btn">follow</button>
+                </div>
+            </div>
+            <div class="profile-card">
+                <div class="profile-pic">
+                    <img src="img/cover 10.png" alt="">
+                </div>
+                <div>
+                    <p class="username">username</p>
+                    <p class="sub-text">followed by user</p>
+                    <button class="action-btn">follow</button>
+                </div>
+            </div>
+            <div class="profile-card">
+                <div class="profile-pic">
+                    <img src="img/cover 11.png" alt="">
+                </div>
+                <div>
+                    <p class="username">username</p>
+                    <p class="sub-text">followed by user</p>
+                    <button class="action-btn">follow</button>
+                </div>
+            </div>
+            <div class="profile-card">
+                <div class="profile-pic">
+                    <img src="img/cover 12.png" alt="">
+                </div>
+                <div>
+                    <p class="username">username</p>
+                    <p class="sub-text">followed by user</p>
+                    <button class="action-btn">follow</button>
+                </div>
+            </div>
+            <div class="profile-card">
+                <div class="profile-pic">
+                    <img src="img/cover 13.png" alt="">
+                </div>
+                <div>
+                    <p class="username">username</p>
+                    <p class="sub-text">followed by user</p>
+                    <button class="action-btn">follow</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     </section>
     <nav class="options-bar">
         <nav class="nav-wrapper">
