a7060271b0f3031ae40c6cc94dabf633ef0ec395
S-furi <stefano.furi@studio.unibo.it>
2022-12-08 00:30:55 +0100

    Testing login session checks.

--- website/post-creation.php
+++ website/post-creation.php
@@ -4,6 +4,11 @@ require_once('../database/db_functions.php');
 
 sec_session_start();
 
+if (!isStillLoggedIn($mysqli)) {
+    header("Location: login.php");
+    echo "Sessione scaduta";
+}
+
 $templateParams["Titolo"] = "Brogram - Post Creation";
 $templateParams["nome"] = "post-creation-t.php";
 
