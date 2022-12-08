868046a60f874d31e66b27199d84d69c322ab24f
S-furi <stefano.furi@studio.unibo.it>
2022-12-08 16:14:01 +0100

    Added simple logout functionality

---
+++ website/logout.php
@@ -0,0 +1,12 @@
+<?php 
+require_once("../database/db_functions.php");
+// http://paololatella.blogspot.com/2017/10/lezione-nov-2017-script-sicuro-per-il.html
+sec_session_start();
+
+$_SESSION = array();
+
+$params = session_get_cookie_params();
+setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+session_destroy();
+header("Location: ./login.php");
+?>
