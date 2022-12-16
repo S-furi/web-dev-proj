771e2bd9959b76acd5ee1aee4fbffe167a558dca
S-furi <stefano.furi@studio.unibo.it>
2022-12-16 16:51:32 +0100

    Changed logout reference to login page

--- website/logout.php
+++ website/logout.php
@@ -8,5 +8,5 @@ $_SESSION = array();
 $params = session_get_cookie_params();
 setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
 session_destroy();
-header("Location: ./login.php");
+header("Location: ./template/login-form.php");
 ?>
