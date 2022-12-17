0e04a237a7bf087eecfcb152a9a05b9d1530fc35
S-furi <stefano.furi@studio.unibo.it>
2022-12-17 18:20:34 +0100

    Temporary function for user suggestion follow btn

--- website/js/home.js
+++ website/js/home.js
@@ -2,7 +2,19 @@ function showForm() {
   window.location.href = "post-creation.php"
 }
 
+function followUser(user, followed) {
+  console.log(user + " " + followed);
+  // follow went ok
+  document.querySelector(".right li.user-suggestion.usr-"+followed + " input").value = "✔️";
+  document.querySelector(".right li.user-suggestion.usr-"+followed).classList.add("disappearing-card");
+  // asynchronous :)
+  setTimeout(() => document.querySelector(".right li.user-suggestion.usr-"+followed).remove(), 1000);
+}
+
 document.getElementById("create-post-xl")
   .addEventListener('click', () => showForm());
 document.getElementById("create-post-sm")
   .addEventListener('click', () => showForm());
+
+
+
