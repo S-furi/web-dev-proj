b8507b58a7e1e549791a152d4421ded0c0340e6f
S-furi <stefano.furi@studio.unibo.it>
2022-12-17 16:29:51 +0100

    made template/login-form.php = login.php

--- website/login.php
+++ website/login.php
@@ -1,20 +1,44 @@
-<?php
-require_once('../database/db_connect.php');
-require_once('../database/db_functions.php');
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../styles/login.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet" />
+    <title>Brogram - Login</title>
+</head>
 
-sec_session_start();
-
-$templateParams["Titolo"] = "Brogram - Login";
-$templateParams["nome"] = "login-form.php";
-
-if (isset($_POST['email'], $_POST['password'])) {
-  if (login($_POST['email'], $_POST['password'], $mysqli)) {
-    header("Location: index.php");
-  } else {
-    echo "login fallito!";
-  }
-}
-
-require("template/base.php");
-
-?>
+<body>
+    <main>
+        <input type="checkbox" id="checkbox" aria-hidden="true" />
+        <section class="signup">
+            <form action="POST">
+                <label for="checkbox" aria-hidden="true">Registrati</label>
+                <label for="first-name"><input type="text" name="first-name" id="first-name" placeholder="Nome"
+                        required="" /></label>
+                <label for="last-name"><input type="text" name="last-name" id="last-name" placeholder="Cognome"
+                        required="" /></label>
+                <label for="username"><input type="text" name="username" id="username" placeholder="Username"
+                        required="" /></label>
+                <label for="email"><input type="email" name="email" id="email" placeholder="Email"
+                        required="" /></label>
+                <label for="password"><input type="password" name="password" id="password" placeholder="Password"
+                        required="" /></label>
+                <input type="button" value="Registrati" id="signup-btn">
+            </form>
+        </section>
+        <section class="login">
+            <form action="POST">
+                <label for="checkbox" aria-hidden="true">Login</label>
+                <label for="email"><input type="email" name="email" id="email" placeholder="Email"
+                        required="" /></label>
+                <label for="password"><input type="password" name="password" id="password" placeholder="Password"
+                        required="" /></label>
+               <input type="button" value="Accedi" id="login-btn" />
+            </form>
+        </section>
+    </main>
+<script src="js/login-check.js"></script>
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+</body>
