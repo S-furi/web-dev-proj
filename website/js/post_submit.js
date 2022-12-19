47d868bbb31b20ddae707144acb65ccb66386794
S-furi <stefano.furi@studio.unibo.it>
2022-12-19 19:31:59 +0100

    Made input file work!

--- website/js/post_submit.js
+++ website/js/post_submit.js
@@ -3,13 +3,14 @@ function fillMain() {
 
   const form = `
             <h1>Creazione Post</h1>
-            <form action="POST">
+            <p></p>
+            <form action="post-creation.php" method="post" enctype="multipart/form-data">
                 <ul>
                   <li>
                       <label for="title">Titolo Post</label><input type="text" name="title" id="title" />
                   </li>
                   <li>
-                      <label for="photo">Foto</label><input type="file" name="photo" id="photo">
+                      <label for="image">Foto</label><input type="file" name="photo" id="image" />
                   </li>
                   <li>
                       <label for="description">Descrizione</label><input type="text" id="description" name="description"/>
@@ -31,53 +32,52 @@ function fillMain() {
 
 fillMain();
 
+// cancel button, redirects to home
 document.querySelector("input[name=cancel-button]")
 .addEventListener('click', () => document.location.href = "index.php");
 
-
-
+// before submitting the form, check fields
 document.querySelector("input[name=creation-button]")
-  .addEventListener('click', function () {
-    const post_params = {
-      "title": document.querySelector("input#title").value,
-      "photo": document.querySelector("input#photo").value,
-      "description": document.querySelector("input#description").value,
-      "location": document.querySelector("input#location").value,
-      "event-datetime": document.querySelector("input#event-datetime").value,
-    }
+    .addEventListener('click', function () {
+        const post_params = {
+            "title": document.querySelector("input#title").value,
+            "photo": document.querySelector("input#image").value,
+            "description": document.querySelector("input#description").value,
+            "location": document.querySelector("input#location").value,
+            "event-datetime": document.querySelector("input#event-datetime").value,
+        }
 
-    if (checkForm(post_params)) {
-      sendPostData(post_params);
-    } else {
-      alert('Filla tutti i campi scemo');
-    }
-  });
+        if (checkForm(post_params)) {
+            document.querySelector(".middle form").submit();
+        } else {
+            document.querySelector(".middle p").innerText = "Riempi tutti i campi.";
+        }
+    });
 
 function checkForm(post_params) {
   let err = true;
   for (const [key, value] of Object.entries(post_params)) {
     if (value == "") {
-      // document.querySelector("input#"+key+">p")
       err = false;
     }
   }
   return err;
 }
 
-function sendPostData(post_params) {
-  const formData = new FormData();
-  for (const [key, value] of Object.entries(post_params)) {
-    formData.append(key, value);
-  }
-  axios.post('api-post.php', formData)
-    .then(res => {
-      console.log(res.data);
-      if (res.data["loginok"]) {
-        alert("TUTTO OK FRATE");
-        window.location.href = "index.php"
-      } else {
-        console.log("QULCOSA storto");
-        console.log(res.data["errormsg"]);
-      }
-    })
-}
+// function sendPostData(post_params) {
+//   const formData = new FormData();
+//   for (const [key, value] of Object.entries(post_params)) {
+//     formData.append(key, value);
+//   }
+//   axios.post('api-post.php', formData)
+//     .then(res => {
+//       console.log(res.data);
+//       if (res.data["loginok"]) {
+//         alert("TUTTO OK FRATE");
+//         window.location.href = "index.php"
+//       } else {
+//         console.log("QULCOSA storto");
+//         console.log(res.data["errormsg"]);
+//       }
+//     })
+// }
