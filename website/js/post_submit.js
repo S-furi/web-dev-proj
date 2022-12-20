92d5a0adb9a71b14356858f536ae2f7e243dbee2
S-furi <stefano.furi@studio.unibo.it>
2022-12-20 19:56:10 +0100

    Refactored form validity check

--- website/js/post_submit.js
+++ website/js/post_submit.js
@@ -5,19 +5,19 @@ function fillMain() {
             <form action="post-creation.php" method="post" enctype="multipart/form-data">
                 <ul>
                   <li>
-                      <label for="title">Titolo Post</label><input type="text" name="title" id="title" />
+                      <label for="title">Titolo Post</label><input type="text" name="title" id="title" required />
                   </li>
                   <li>
-                      <label for="image">Foto</label><input type="file" name="photo" id="image" />
+                      <label for="image">Foto</label><input type="file" name="photo" id="image" required />
                   </li>
                   <li>
-                      <label for="description">Descrizione</label><input type="text" id="description" name="description"/>
+                      <label for="description">Descrizione</label><input type="text" id="description" name="description" required/>
                   </li>
                   <li>
-                      <label for="location">Luogo</label><input type="text" id="location" name="location" />
+                      <label for="location">Luogo</label><input type="text" id="location" name="location" required />
                   </li>
                   <li>
-                      <label for="event-datetime">Data e Ora dell'Evento</label><input type="datetime-local" name="event-datetime" id="event-datetime" />
+                      <label for="event-datetime">Data e Ora dell'Evento</label><input type="datetime-local" name="event-datetime" id="event-datetime" required />
                   </li> 
                 </ul>
               <input type="button" name="cancel-button" value="Annulla" class="btn" />
@@ -35,23 +35,12 @@ document.querySelector("input[name=cancel-button]")
 
 // before submitting the form, check fields
 document.querySelector("input[name=creation-button]")
-    .addEventListener('click', function () {
-        const data = new FormData(document.querySelector(".main form"));
-        const post_params = Object.fromEntries(data.entries());
-
-        if (checkForm(post_params)) {
-            document.querySelector(".middle form").submit();
-        } else {
-            document.querySelector(".middle p").innerText = "Riempi tutti i campi.";
-        }
-    });
-
-function checkForm(post_params) {
-  let err = true;
-  for (const [key, value] of Object.entries(post_params)) {
-    if (value === null || value === "") {
-      err = false;
+.addEventListener('click', function () {
+    const form = document.querySelector(".main form")
+    
+    if (form.checkValidity()) {
+        document.querySelector(".middle form").submit();
+    } else {
+        document.querySelector(".middle p").innerText = "Riempi tutti i campi.";
     }
-  }
-  return err;
-}
+});
