a88f536ac0d89e3f213ecc3b8fa4ef394f877271
S-furi <stefano.furi@studio.unibo.it>
2022-12-22 15:00:20 +0100

    First implementation of a simple calendar

---
+++ website/js/calendar.js
@@ -0,0 +1,88 @@
+function isToday(day) {
+    const todayDate = new Date();
+    return (day == date.getDate() && currentMonth == todayDate.getMonth() && currentYear == todayDate.getFullYear())
+        ? ` class="active"` : "";
+}
+
+function renderCalendar() {
+    // first day index of current month
+    const firstDayIndex = new Date(currentYear, currentMonth, 1).getDay();
+
+    // last day index of current month
+    const lastDayCurMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+    const lastDayIndex = new Date(currentYear, currentMonth, lastDayCurMonth).getDay();
+
+    // get last month's last day number
+    const lastMonthLastDay = new Date(currentYear, currentMonth, 0).getDate();
+
+    let htmlDayTag = "";
+    // fill last days of last month
+    for (let i = firstDayIndex; i > 0; i--) {
+        htmlDayTag += `<li class="inactive">${lastMonthLastDay - i + 1}</li>`
+    }
+
+    // fill the rest of current month's days
+    for (let i = 1; i <= lastDayCurMonth; i++) {
+        htmlDayTag += `<li${isToday(i)}>${i}</li>`;
+    }
+
+    // filling next months first days
+    for (let i = lastDayIndex; i < 6; i++) {
+        htmlDayTag += `<li class="inactive">${i - lastDayIndex + 1}</li>`
+    }
+
+    document.querySelector(".current-date").innerHTML = `${months[currentMonth]} ${currentYear}`
+    document.querySelector(".days").innerHTML = htmlDayTag;
+}
+
+function handleMonthChange(event) {
+    currentMonth = event.target.id == "prev" ? currentMonth - 1 : currentMonth + 1;
+    if (currentMonth < 0 || currentMonth > 11) {
+        date = new Date(currentYear, currentMonth);
+        currentYear = date.getFullYear();
+        currentMonth = date.getMonth();
+    } else {
+        date = new Date();
+    }
+    renderCalendar();
+}
+
+
+function insertCalendar() {
+    const wrapperContent = `
+            <div class="calendar-head">
+                <p class="current-date">Settembre 2022</p>
+                <div class="icons">
+                    <span id="prev" class="material-symbols-outlined">chevron_left</span>
+                    <span id="next" class="material-symbols-outlined">chevron_right</span>
+                </div>
+            </div>
+            <div class="calendar-body">
+                <ul class="weeks">
+                </ul>
+                <ul class="days">
+                </ul>
+            </div>`;
+
+    document.querySelector(".right .calendar-wrapper").innerHTML = wrapperContent;
+}
+
+// they'll be modified if prev or next icons are clicked
+let date = new Date();
+let currentMonth = date.getMonth();
+let currentYear = date.getFullYear();
+
+
+const months = [
+    "Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio",
+    "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
+
+insertCalendar();
+
+// make the chevrons change months
+document.querySelectorAll(".right .calendar-wrapper .icons span").forEach(icon => {
+    icon.addEventListener('click', event => handleMonthChange(event));
+});
+
+// finally, render the calendar
+renderCalendar();
