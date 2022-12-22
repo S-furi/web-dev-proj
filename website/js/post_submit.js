031dfa86339bc055115b68d2a2ea1b9f4cb653dd
S-furi <stefano.furi@studio.unibo.it>
2022-12-22 12:15:09 +0100

    wrapping post creation form in a separate div

--- website/js/post_submit.js
+++ website/js/post_submit.js
@@ -1,28 +1,30 @@
 function fillMain() {
     const form = `
-            <h1>Creazione Post</h1>
-            <p></p>
-            <form action="post-creation.php" method="post" enctype="multipart/form-data">
-                <ul>
-                  <li>
-                      <label for="title">Titolo Post</label><input type="text" name="title" id="title" required />
-                  </li>
-                  <li>
-                      <label for="image">Foto</label><input type="file" name="photo" id="image" required />
-                  </li>
-                  <li>
-                      <label for="description">Descrizione</label><input type="text" id="description" name="description" required/>
-                  </li>
-                  <li>
-                      <label for="location">Luogo</label><input type="text" id="location" name="location" required />
-                  </li>
-                  <li>
-                      <label for="event-datetime">Data e Ora dell'Evento</label><input type="datetime-local" name="event-datetime" id="event-datetime" required />
-                  </li> 
-                </ul>
-              <input type="button" name="cancel-button" value="Annulla" class="btn" />
-              <input type="button" name="creation-button" value="Crea" class="btn btn-primary"/>
-            </form> `;
+            <div class="wrapper">
+                <h1>Creazione Post</h1>
+                <p></p>
+                <form action="post-creation.php" method="post" enctype="multipart/form-data">
+                    <ul>
+                      <li>
+                          <label for="title">Titolo Post</label><input type="text" name="title" id="title" required />
+                      </li>
+                      <li>
+                          <label for="image">Foto</label><input type="file" name="photo" id="image" required />
+                      </li>
+                      <li>
+                          <label for="description">Descrizione</label><input type="text" id="description" name="description" required/>
+                      </li>
+                      <li>
+                          <label for="location">Luogo</label><input type="text" id="location" name="location" required />
+                      </li>
+                      <li>
+                          <label for="event-datetime">Data e Ora dell'Evento</label><input type="datetime-local" name="event-datetime" id="event-datetime" required />
+                      </li> 
+                    </ul>
+                  <input type="button" name="cancel-button" value="Annulla" class="btn" />
+                  <input type="button" name="creation-button" value="Crea" class="btn btn-primary"/>
+        </form> 
+    </div>`;
 
     document.querySelector("main .middle").innerHTML = form;
 }
