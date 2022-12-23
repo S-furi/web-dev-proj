e37a42b57129936685413c38d08ba9fb1b4fc34a
S-furi <stefano.furi@studio.unibo.it>
2022-12-23 18:33:00 +0100

    Updated form

--- website/template/post-creation-t.php
+++ website/template/post-creation-t.php
@@ -1,26 +1,25 @@
-        <form action="#" method="post" name="signup_form">
-            <h2>Creazione Post</h2>
+    <div class="wrapper">
+        <h1>Creazione Post</h1>
+        <p><?php if (isset($_GET["err"])) echo $_GET["err"]; ?></p>
+        <form action="api/api-post.php" method="post" enctype="multipart/form-data">
             <ul>
-                <!-- <li> -->
-                    <!-- <label for="profilepic">Profile Picture:</label><input type="file" id="profilepic"  me="profilepic" /> -->
-                <!-- </li> -->
-                <li>
-                    <label for="title">Titolo Post:</label><input type="text" name="title" id="title">
-                </li>
-                <li>
-                    <label for="photo">Foto:</label><input type="file" name="photo" id="photo">
-                </li>
-                <li>
-                    <label for="description">Descrizione:</label><input type="text" id="description" name="description"/>
-                </li>
-                <li>
-                    <label for="location">Luogo:</label><input type="text" id="location" name="location" />
-                </li>
-                    <label for="event-datetime">Data e Ora dell'Evento: </label><input type="datetime-local" name="event-datetime" id="event-datetime">
-                    <p style="color: red; display: inline" id="date-error-msg"></p>
-                </li> 
-                <li>
-                    <input type="button" name="creation-button" value="Create" />
-                </li>
+              <li>
+                  <label for="title">Titolo Post</label><input type="text" name="title" id="title" required />
+              </li>
+              <li>
+                  <label for="image">Foto</label><input type="file" name="photo" id="image" required />
+              </li>
+              <li>
+                  <label for="description">Descrizione</label><input type="text" id="description" name="description" required/>
+              </li>
+              <li>
+                  <label for="location">Luogo</label><input type="text" id="location" name="location" required />
+              </li>
+              <li>
+                  <label for="event-datetime">Data e Ora dell'Evento</label><input type="datetime-local" name="event-datetime" id="event-datetime" required />
+              </li> 
             </ul>
-        </form>
+          <input type="button" name="cancel-button" value="Annulla" class="btn" onclick="window.location.href='index.php'" />
+          <input type="button" name="creation-button" value="Crea" class="btn btn-primary" onclick="form.submit()"/>
+        </form> 
+    </div>
