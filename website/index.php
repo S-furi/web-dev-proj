6659895b14158b18d0158e7518e0cafae39e634e
S-furi <stefano.furi@studio.unibo.it>
2022-12-29 16:30:58 +0100

    Fixed date format

--- website/index.php
+++ website/index.php
@@ -7,9 +7,12 @@ $templateParams["Titolo"] = "Brogram - Home";
 $templateParams["template_name"] = "timeline.php";
 $templateParams["posts"]= getFriendsPosts($_SESSION["user_id"], $mysqli);
 
+
+
 // fixing images path
 for($i = 0; $i < count($templateParams["posts"]); $i++) {
      $templateParams["posts"][$i]["image"] = UPLOAD_POST_DIR.$templateParams["posts"][$i]["image"];
+    $templateParams["posts"][$i]["eventDate"]= date("d-m-Y H:i", strtotime($templateParams["posts"][$i]['eventDate']));
 }
 
 require("template/base.php");
