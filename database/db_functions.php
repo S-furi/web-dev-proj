870a31e8deededf7b305f7c96d9d352f1a9c7f9b
S-furi <stefano.furi@studio.unibo.it>
2022-12-31 01:15:43 +0100

    New functions for events and participations

--- database/db_functions.php
+++ database/db_functions.php
@@ -124,10 +124,41 @@ function insertParticipant($usrId, $eventId, mysqli $mysqli) {
     $stmt = $mysqli->prepare("INSERT INTO participations (usrId, eventId) VALUES (?, ?);");
     $stmt->bind_param("ii", $usrId, $eventId);
     if ($stmt->execute()) {
-        $stmt = $mysqli->prepare("UPDATE events SET events.participants = events.participants + 1 WHERE events.eventId = ?;");
-        $stmt->bind_param("i", $eventId);
-        return $stmt->execute();
+        return incrementParticipants($eventId, $mysqli);
     }
+}
+
+function incrementParticipants($eventId, $mysqli) {
+    $stmt = $mysqli->prepare("UPDATE events SET events.participants = events.participants + 1 WHERE events.eventId = ?;");
+    $stmt->bind_param("i", $eventId);
+    return $stmt->execute();
+}
+
+function insertParticipantFromPostId($usrId, $postId, mysqli $mysqli) {
+    $query = "INSERT INTO participations VALUES (?, ?);";
+    $stmt = $mysqli->prepare($query);
+    $eventId = getEventFromPost($postId, $mysqli)["eventId"];
+    $stmt->bind_param("ii", $usrId, $eventId);
+    return $stmt->execute() && incrementParticipants($eventId, $mysqli);
+}
+
+function isUserParticipating($usrId, $postId, mysqli $mysqli) {
+    $query = "SELECT * FROM participations WHERE usrId = ? AND eventId = ?;";
+    $stmt = $mysqli->prepare($query);
+    $eventId = getEventFromPost($postId, $mysqli)["eventId"];
+    $stmt->bind_param("ii", $usrId, $eventId);
+    $stmt->execute();
+
+    $res = $stmt->get_result()->fetch_all();
+    return count($res) > 0;
+}
+
+function getEventFromPost($postId, mysqli $mysqli) {
+    $query = "SELECT eventId FROM events JOIN posts ON (events.postId = posts.postId) WHERE posts.postId = ?";
+    $stmt = $mysqli->prepare($query);
+    $stmt->bind_param("i", $postId);
+    $stmt->execute();
+    return $stmt->get_result()->fetch_all(MYSQLI_ASSOC)[0];
 }
 
 function getPosts($usrId, $mysqli) {
