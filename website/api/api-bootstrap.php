09e0b681d258b73abd1a5b057fed0b5b610faf84
S-furi <stefano.furi@studio.unibo.it>
2022-12-31 18:43:50 +0100

    Removed session check (conflict in login)

--- website/api/api-bootstrap.php
+++ website/api/api-bootstrap.php
@@ -3,12 +3,8 @@ require_once('../../database/db_connect.php');
 require_once('../../database/db_functions.php');
 require_once('../../utils/utils_functions.php');
 
-define("UPLOAD_POST_DIR", "../img/posts/");
-
 sec_session_start();
 
-if (!checkUserSession($mysqli)) {
-    header("Location: login.php");
-}
+define("UPLOAD_POST_DIR", "../img/posts/");
 
 ?>
