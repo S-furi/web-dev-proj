cd8229a60db7c3fbfc097cc0c600525bf4b6ebb0
S-furi <stefano.furi@studio.unibo.it>
2022-12-31 18:45:17 +0100

    Implemeted like buttons style

--- website/template/base.php
+++ website/template/base.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="../styles/post-creation.css" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="../styles/calendar.css" />
+    <link rel="stylesheet" href="../styles/like-transitions.css" />
     <title>Brogram - Home</title>
 </head>
 
