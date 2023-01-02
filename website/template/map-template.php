2a785dd8657bdfab1d97f7eff8bdded8dac4a69b
S-furi <stefano.furi@studio.unibo.it>
2023-01-02 23:26:13 +0100

    Added template

--- website/template/map-template.php
+++ website/template/map-template.php
@@ -4,4 +4,8 @@
         <label for="search-button"><button type="button" id="search-button" name="search-button" class="btn btn-primary" onclick="osmSearch()">Cerca</button></label>
     </div>
     <div id="mapdiv" style="width: 100%; height: 100%;"></div>
+    <div id="popup" class="ol-popup">
+        <a href="#" id="popup-closer" class="ol-popup-closer"></a>
+        <div id="popup-content"></div>
+    </div>
 </div>
