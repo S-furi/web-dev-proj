fa8bd1aaa8427ba255a44078c9179db796c7b502
S-furi <stefano.furi@studio.unibo.it>
2023-01-03 01:15:30 +0100

    Implemented active effect when radar is selected

--- website/map.php
+++ website/map.php
@@ -2,6 +2,7 @@
 require_once("bootstrap.php");
 
 $templateParams["template_name"] = "map-template.php";
+$templateParams["active-radar"] = "active";
 array_push($templateParams["js"], "https://openlayers.org/en/v5.3.0/build/ol.js", "js/map.js");
 
 require("template/base.php");
