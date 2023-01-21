53e070cc1d359f1f328995f54ef5174bf2318498
Michele Montesi <michele.montesi3@studio.unibo.it>
2023-01-21 18:55:25 +0100

    user info dynamic in user profile, timeline, posts

--- website/user-profile.php
+++ website/user-profile.php
@@ -6,6 +6,13 @@ $posts = getPosts(intval($_GET['usrId']), $mysqli);
 $followers_n = getFollowersNum($user['usrId'], $mysqli);
 $following_n = getFollowingNum($user['usrId'], $mysqli);
 
+if (checkUserInfoExists($_GET['usrId'], $mysqli)) {
+  $userInfo = getUserInfo($_GET['usrId'], $mysqli);
+  $userInfo[0]['profileImg'] = 'img/' . $user['username'] . "/propic/" . $userInfo[0]['profileImg'];
+} else {
+  $userInfo[0] = array('bio' => '', 'profileImg' => 'img/no-profile-pic.png');
+}
+
 $templateParams['Titolo'] = "Brogram - Profilo " . $templateParams['user']['username'];
 $templateParams['template_name'] = "user-profile-template.php";
 
