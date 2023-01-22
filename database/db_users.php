3cda49bc847741a67fb384589fdb1e746414a77b
S-furi <stefano.furi@studio.unibo.it>
2023-01-22 11:05:29 +0100

    Final version of user suggestion queries

--- database/db_users.php
+++ database/db_users.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file contains Users and followers utility methotds
  * for retrieving information from database.
@@ -6,63 +7,66 @@
 
 function insertNewUser($email, $username,  $password, $first_name, $last_name, $mysqli): bool
 {
-    // We are using password_hash() because it's safe and simple to use.
-    // It uses a secure salt for hashing the password, and the 
-    // algorithm used changes overtime using PASSWORD_DEFAULT,
-    // reulting in the most secure algorithm available at 
-    // the current version of PHP.
-    $password = password_hash($password, PASSWORD_DEFAULT);
+  // We are using password_hash() because it's safe and simple to use.
+  // It uses a secure salt for hashing the password, and the 
+  // algorithm used changes overtime using PASSWORD_DEFAULT,
+  // reulting in the most secure algorithm available at 
+  // the current version of PHP.
+  $password = password_hash($password, PASSWORD_DEFAULT);
 
-    $stmt = $mysqli->prepare("INSERT INTO users (email, password, username, firstName, lastName) VALUES (?, ?, ?, ?, ?)");
-    $stmt->bind_param("sssss", $email, $password, $username, $first_name, $last_name);
+  $stmt = $mysqli->prepare("INSERT INTO users (email, password, username, firstName, lastName) VALUES (?, ?, ?, ?, ?)");
+  $stmt->bind_param("sssss", $email, $password, $username, $first_name, $last_name);
 
-    // An exeption is thrown in the case where email or username are already in DB
-    // (and of course all the cases where db queries fails for other internal reasons)
-    try {
-        $stmt->execute();
-    } catch (\Throwable $th) {
-        return false;
-    }
-    return true;
+  // An exeption is thrown in the case where email or username are already in DB
+  // (and of course all the cases where db queries fails for other internal reasons)
+  try {
+    $stmt->execute();
+  } catch (\Throwable $th) {
+    return false;
+  }
+  return true;
 }
 
-function insertNewUserInfo($usrId, $bio, $propic, $mysqli) {
+function insertNewUserInfo($usrId, $bio, $propic, $mysqli)
+{
   $query = "INSERT INTO user_profile (users_usrId, bio, profileImg) VALUES (?, ?, ?)";
   $stmt = $mysqli->prepare($query);
   $stmt->bind_param("iss", $usrId, $bio, $propic);
 
   try {
-      $stmt->execute();
+    $stmt->execute();
   } catch (\Throwable $th) {
-      return false;
+    return false;
   }
   return true;
 }
 
-function updateProfileInfo($usrId, $bio, $propic, $mysqli) {
+function updateProfileInfo($usrId, $bio, $propic, $mysqli)
+{
   $query = "UPDATE user_profile SET bio=?, profileImg=? WHERE users_usrId=?";
   $stmt = $mysqli->prepare($query);
   $stmt->bind_param("ssi", $bio, $propic, $usrId);
 
   try {
-      $stmt->execute();
+    $stmt->execute();
   } catch (\Throwable $th) {
-      return false;
+    return false;
   }
   return true;
 }
 
 function getUser($usrId, $mysqli)
 {
-    $query = "SELECT usrId, email, username, firstName, lastName FROM users WHERE usrId=?";
-    $stmt = $mysqli->prepare($query);
-    $stmt->bind_param("i", $usrId);
+  $query = "SELECT usrId, email, username, firstName, lastName FROM users WHERE usrId=?";
+  $stmt = $mysqli->prepare($query);
+  $stmt->bind_param("i", $usrId);
 
-    $stmt->execute();
-    return $stmt->get_result()->fetch_all(MYSQLI_ASSOC)[0];
+  $stmt->execute();
+  return $stmt->get_result()->fetch_all(MYSQLI_ASSOC)[0];
 }
 
-function getUserInfo($usrId, $mysqli) {
+function getUserInfo($usrId, $mysqli)
+{
   $query = "SELECT bio, profileImg FROM user_profile WHERE users_usrId = ?";
   $stmt = $mysqli->prepare($query);
   $stmt->bind_param("i", $usrId);
@@ -71,7 +75,8 @@ function getUserInfo($usrId, $mysqli) {
   return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 }
 
-function checkUserInfoExists($usrId, $mysqli) {
+function checkUserInfoExists($usrId, $mysqli)
+{
   $query = "SELECT COUNT(1) FROM user_profile WHERE users_usrId = ?";
   $stmt = $mysqli->prepare($query);
   $stmt->bind_param("i", $usrId);
@@ -83,32 +88,70 @@ function checkUserInfoExists($usrId, $mysqli) {
 /**
  * Returns friends of friends.
  *
- * THIS IS A TEMPORARY VERSION, returns first 5 users in db
- * not followed by provided usrId
+ * Returns first 5 users in db not followed by provided usrId
  */
 function getSuggestedUser($usrId, $mysqli)
 {
-    // Note: if DB is big `ORDER BY RAND()` will have bad performances
-    $query =
-        "SELECT usrId, username, firstName, lastName 
-        FROM users 
-        WHERE usrId <> ? 
-        AND usrId NOT IN (SELECT friendId FROM followers where usrId = ?)
-        ORDER BY RAND() LIMIT 3";
+  $suggestionNum = 5; // how many suggestions are fetched
 
-    $stmt = $mysqli->prepare($query);
-    $stmt->bind_param("ii", $usrId, $usrId);
-    $stmt->execute();
-    return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+  $suggUsers = getFriendSFriends($usrId, $suggestionNum, $mysqli);
+
+  if (count($suggUsers) < $suggestionNum) {
+    array_merge(
+      $suggUsers,
+      getRandomUsers($usrId, $suggestionNum - count($suggUsers), $mysqli)
+    );
+  } else {
+    array_slice($suggUsers, 0, $suggestionNum, true);
+  }
+
+  return $suggUsers;
+}
+
+
+function getRandomUsers($usrId, $suggestionNum, mysqli $mysqli)
+{
+  // Note: if DB is big `ORDER BY RAND()` will have bad performances
+  $query =
+    "SELECT usrId, username, firstName, lastName 
+    FROM users 
+    WHERE usrId <> ? 
+    AND usrId NOT IN (SELECT friendId FROM followers where usrId = ?)
+    ORDER BY RAND() LIMIT ?";
+
+
+  $stmt = $mysqli->prepare($query);
+  $stmt->bind_param("iii", $usrId, $usrId, $suggestionNum);
+  $stmt->execute();
+  return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+}
+
+function getFriendSFriends($usrId, $suggestionNum, mysqli $mysqli)
+{
+  $query =
+    "SELECT DISTINCT users.* FROM users
+      JOIN followers ON users.usrId = followers.friendId
+      WHERE users.usrId NOT IN (SELECT friendId FROM followers WHERE usrId = ?)
+      AND followers.usrId IN (SELECT friendId FROM followers WHERE usrId = ?)
+      AND users.usrId != ?
+      ORDER BY RAND()
+      LIMIT ?;";
+
+  $stmt = $mysqli->prepare($query);
+  $stmt->bind_param("iiii", $usrId, $usrId, $usrId, $suggestionNum);
+
+  $stmt->execute();
+
+  return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 }
 
 function getUserFromFragments($queryFragment, $mysqli)
 {
-    // removing special chars
-    $condition = preg_replace('/[^A-Za-z0-9\- ]/', '', $queryFragment);
-    $condition = '%' . $condition . '%';
+  // removing special chars
+  $condition = preg_replace('/[^A-Za-z0-9\- ]/', '', $queryFragment);
+  $condition = '%' . $condition . '%';
 
-    $query = "
+  $query = "
         SELECT email, username, usrId, firstName, lastName 
         FROM users 
         WHERE username LIKE ?
@@ -117,71 +160,69 @@ function getUserFromFragments($queryFragment, $mysqli)
         ORDER BY usrId DESC
         LIMIT 10;";
 
-    $stmt = $mysqli->prepare($query);
-    $stmt->bind_param('sss', $condition, $condition, $condition);
-    $stmt->execute();
+  $stmt = $mysqli->prepare($query);
+  $stmt->bind_param('sss', $condition, $condition, $condition);
+  $stmt->execute();
 
-    return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+  return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 }
 
 function insertNewFollower($user, $followed, $mysqli)
 {
-    $stmt = $mysqli->prepare("INSERT INTO followers (usrId, friendId) VALUES (?, ?)");
-    // friendships aren't bidirectional
-    $stmt->bind_param("ii", $user, $followed);
+  $stmt = $mysqli->prepare("INSERT INTO followers (usrId, friendId) VALUES (?, ?)");
+  // friendships aren't bidirectional
+  $stmt->bind_param("ii", $user, $followed);
 
-    try {
-        $stmt->execute();
-    } catch (mysqli_sql_exception $e) {
-        return false;
-    }
-    return true;
+  try {
+    $stmt->execute();
+  } catch (mysqli_sql_exception $e) {
+    return false;
+  }
+  return true;
 }
 
 function getFollowedUsers($user, $mysqli)
 {
-    $stmt = $mysqli->prepare("SELECT friendId FROM followers WHERE usrId = ?");
-    $stmt->bind_param("i", $user);
-    $stmt->execute();
+  $stmt = $mysqli->prepare("SELECT friendId FROM followers WHERE usrId = ?");
+  $stmt->bind_param("i", $user);
+  $stmt->execute();
 
-    return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+  return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 }
 
 function getFollowingUsers($user, $mysqli)
 {
-    $stmt = $mysqli->prepare("SELECT usrId FROM followers WHERE friendId = ?");
-    $stmt->bind_param("i", $user);
-    $stmt->execute();
+  $stmt = $mysqli->prepare("SELECT usrId FROM followers WHERE friendId = ?");
+  $stmt->bind_param("i", $user);
+  $stmt->execute();
 
-    return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+  return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 }
 
 function getFollowingNum($userId, $mysqli)
 {
-    $stmt = $mysqli->prepare("SELECT COUNT(*) as follow_num FROM followers WHERE usrId = ?");
-    $stmt->bind_param("i", $userId);
-    $stmt->execute();
+  $stmt = $mysqli->prepare("SELECT COUNT(*) as follow_num FROM followers WHERE usrId = ?");
+  $stmt->bind_param("i", $userId);
+  $stmt->execute();
 
-    return $stmt->get_result()->fetch_all(MYSQLI_ASSOC)[0]['follow_num'];
+  return $stmt->get_result()->fetch_all(MYSQLI_ASSOC)[0]['follow_num'];
 }
 
 
 function getFollowersNum($userId, $mysqli)
 {
-    $stmt = $mysqli->prepare("SELECT COUNT(*) as follow_num FROM followers WHERE friendId = ?");
-    $stmt->bind_param("i", $userId);
-    $stmt->execute();
+  $stmt = $mysqli->prepare("SELECT COUNT(*) as follow_num FROM followers WHERE friendId = ?");
+  $stmt->bind_param("i", $userId);
+  $stmt->execute();
 
-    return $stmt->get_result()->fetch_all(MYSQLI_ASSOC)[0]['follow_num'];
+  return $stmt->get_result()->fetch_all(MYSQLI_ASSOC)[0]['follow_num'];
 }
 
 function checkFollow($userId, $friendId, $mysqli)
 {
-    $stmt = $mysqli->prepare("SELECT COUNT(*) as follow_num FROM followers WHERE usrId = ? AND friendId = ?");
-    $stmt->bind_param("ii", $userId, $friendId);
-    $stmt->execute();
-    
-    return $stmt->get_result()->fetch_all(MYSQLI_ASSOC)[0]['follow_num'] > 0;
-}
+  $stmt = $mysqli->prepare("SELECT COUNT(*) as follow_num FROM followers WHERE usrId = ? AND friendId = ?");
+  $stmt->bind_param("ii", $userId, $friendId);
+  $stmt->execute();
 
-?>
+  return $stmt->get_result()->fetch_all(MYSQLI_ASSOC)[0]['follow_num'] > 0;
+}
