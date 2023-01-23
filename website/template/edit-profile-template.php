3bc6176c4a63803a2570842eeccaa16ac191492c
S-furi <stefano.furi@studio.unibo.it>
2023-01-23 15:54:43 +0100

    Fixed redirection to index when done edits

--- website/template/edit-profile-template.php
+++ website/template/edit-profile-template.php
@@ -1,52 +1,7 @@
-<?php 
-$error = "";
-
-$userInfo = getUserInfo($_SESSION['user_id'], $mysqli);
-if (empty($userInfo)) {
-  $bio = "";
-} else {
-  $bio = $userInfo[0]['bio'];
-}
-
-if (isset($_POST['bio']) && $_FILES['propic']['error'] == 0) {
-  $usrId = $_SESSION['user_id'];
-  $bio = $_POST['bio'];
-  $propic = $_FILES['propic'];
-  $username = getUser($usrId, $mysqli)['username'];
-  $propic_dir = IMG_DIR . $username . "/propic/";
-
-  if (!is_dir($propic_dir)) {
-    mkdir($propic_dir, 0777, true);
-  } 
-
-  list($err, $imgPath) = uploadImage($propic_dir, $propic);
-
-  if ($err != 0) {
-    if (checkUserInfoExists($usrId, $mysqli) == 1) {
-      if (!updateProfileInfo($usrId, $bio, $imgPath, $mysqli)) {
-        $error = "C'è stato un errore nell'aggiornamento del profilo, si prega di riprovare più tardi.";
-      } else {
-        $files = glob($propic_dir . '/*');
-        foreach ($files as $file) {
-          if (basename($file) !== $imgPath) {
-            unlink($file);
-          }
-        }
-      }
-    } else {
-      if (!insertNewUserInfo($usrId, $bio, $imgPath, $mysqli)) {
-        $error = "C'è stato un errore nell'inserimento del profilo, si prega di riprovare più tardi.";
-      }
-    }
-  }
-}
-
-?>    
-    
     <div class="wrapper">
         <h1>Modifica Profilo</h1>
         <p><?php if (isset($_GET["err"])) echo $_GET["err"]; ?></p>
-        <form action="" method="post" enctype="multipart/form-data">
+        <form action="edit-profile.php" method="post" enctype="multipart/form-data">
             <ul>
               <li>
                   <label for="propic">Immagine profilo</label><input type="file" name="propic" id="propic" required />
@@ -56,7 +11,7 @@ if (isset($_POST['bio']) && $_FILES['propic']['error'] == 0) {
               </li>
             </ul>
           <input type="button" name="cancel-button" value="Annulla" class="btn" onclick="window.location.href='index.php'" />
-          <input type="button" name="creation-button" value="Modifica" class="btn btn-primary" onclick="form.submit()"/>
+          <input type="submit" name="creation-button" value="Modifica" class="btn btn-primary" />
           
           <div class="error">
             <?php echo $error; ?>
