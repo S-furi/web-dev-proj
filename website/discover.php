458e42890d686fdea6ca793e5d299acc11aa64ab
S-furi <stefano.furi@studio.unibo.it>
2023-01-24 20:51:23 +0100

    Made discover page use dynamic discover

--- website/discover.php
+++ website/discover.php
@@ -5,16 +5,9 @@ $templateParams["Titolo"] = "Brogram - Discover";
 
 $templateParams["template_name"] = "timeline.php";
 $templateParams["active-explore"] = "active";
-$templateParams["posts"] = getDiscoverPosts($_SESSION["user_id"], $mysqli);
 
-// fixing images path
-for($i = 0; $i < count($templateParams["posts"]); $i++) {
-    $username = getUser($templateParams['posts'][$i]['usrId'], $mysqli)['username'];
-    $templateParams["posts"][$i]["image"] = IMG_DIR.$username.'/posts/'.$templateParams["posts"][$i]["image"];
-    $templateParams["posts"][$i]["eventDate"]= date("d-m-Y H:i", strtotime($templateParams["posts"][$i]['eventDate']));
-}
-
-array_push($templateParams["js"], "js/profile.js");
+// inserting the new script, after importing axios
+array_splice($templateParams["js"], 1, 0, "js/timeline.js");
 
 require("template/base.php");
 ?>
