7711fea0272af6f237b01674da5bd6329144da42
S-furi <stefano.furi@studio.unibo.it>
2023-01-24 16:44:57 +0100

    Get the number of participants given a postId

--- database/db_events.php
+++ database/db_events.php
@@ -45,6 +45,16 @@ function insertParticipantFromPostId($usrId, $postId, mysqli $mysqli)
       && notify("participation", $postAuthor, $participationId, $mysqli);
 }
 
+function getParticipants($postId, mysqli $mysqli)
+{
+  $eventId = getEventFromPost($postId, $mysqli);
+  $query = "SELECT * FROM events WHERE eventId = ?";
+  $stmt = $mysqli->prepare($query);
+  $stmt->bind_param("i", $eventId);
+  $stmt->execute();
+  return $stmt->get_result()->fetch_all(MYSQLI_ASSOC)[0]["participants"];
+}
+
 function isUserParticipating($usrId, $postId, mysqli $mysqli)
 {
     $query = "SELECT * FROM participations WHERE usrId = ? AND eventId = ?;";
