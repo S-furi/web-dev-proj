98c81f3267bdc81a4df606ff7ffc05a60334b057
S-furi <stefano.furi@studio.unibo.it>
2023-01-24 17:43:57 +0100

    Retrieving users participating to an event

--- database/db_events.php
+++ database/db_events.php
@@ -60,6 +60,19 @@ function getParticipants($postId, mysqli $mysqli)
   return 0;
 }
 
+function getUsersParticipating($postId, mysqli $mysqli) {
+  $eventId = getEventFromPost($postId, $mysqli)["eventId"];
+  $query = "SELECT *
+              FROM users join participations
+              ON (users.usrId = participations.usrId)
+              WHERE participations.eventId = ?";
+  
+  $stmt = $mysqli->prepare($query);
+  $stmt->bind_param("i", $eventId);
+  $stmt->execute();
+  return $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+}
+
 function isUserParticipating($usrId, $postId, mysqli $mysqli)
 {
     $query = "SELECT * FROM participations WHERE usrId = ? AND eventId = ?;";
