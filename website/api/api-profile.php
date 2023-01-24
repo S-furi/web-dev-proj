b199428548d256863a35672974e7cb8190441468
Michele Montesi <michele.montesi3@studio.unibo.it>
2023-01-24 21:01:33 +0100

    implemented modal to show participants

--- website/api/api-profile.php
+++ website/api/api-profile.php
@@ -26,28 +26,44 @@ function getUsersList($usrId, $mysqli, $action) {
   return $result;
 }
 
-function getUserLikes($postId, $mysqli, $action) {
-  if ($action == 2) {
-    $postId = $_GET['postId'];
-    $list = getUserLikeDetails($postId, $mysqli);
-    foreach ($list as &$user) {
-      if (checkUserInfoExists($user['usrId'], $mysqli)) {
-        $userInfo = getUserInfo($user['usrId'], $mysqli);
-        $user['profileImg'] = 'img/' . $user['username'] . "/propic/" . $userInfo[0]['profileImg'];
-      } else {
-        $user['profileImg'] = 'img/no-profile-pic.png';
-      }
+function getUserLikes($postId, $mysqli) {
+  $result['ok'] = false;
+  $list = getUserLikeDetails($postId, $mysqli);
+  foreach ($list as &$user) {
+    if (checkUserInfoExists($user['usrId'], $mysqli)) {
+      $userInfo = getUserInfo($user['usrId'], $mysqli);
+      $user['profileImg'] = 'img/' . $user['username'] . "/propic/" . $userInfo[0]['profileImg'];
+    } else {
+      $user['profileImg'] = 'img/no-profile-pic.png';
     }
-    $result['likes'] = $list;
-    $result['ok'] = true;
-    return $result;
   }
+  $result['likes'] = $list;
+  $result['ok'] = true;
+  return $result;
+}
+
+function getPostParticipantsUsers($postId, $mysqli) {
+  $result['ok'] = false;
+  $list = getUsersParticipating($postId, $mysqli);
+  foreach ($list as &$user) {
+    if (checkUserInfoExists($user['usrId'], $mysqli)) {
+      $userInfo = getUserInfo($user['usrId'], $mysqli);
+      $user['profileImg'] = 'img/' . $user['username'] . "/propic/" . $userInfo[0]['profileImg'];
+    } else {
+      $user['profileImg'] = 'img/no-profile-pic.png';
+    }
+  }
+  $result['participants'] = $list;
+  $result['ok'] = true;
+  return $result;
 }
 
 if ($_GET['azione'] == 0 || $_GET['azione'] == 1) {
   $result = getUsersList($_GET['usrId'], $mysqli, $_GET['azione']);
 } elseif ($_GET['azione'] == 2) {
-  $result = getUserLikes($_GET['postId'], $mysqli, $_GET['azione']);
+  $result = getUserLikes($_GET['postId'], $mysqli);
+} elseif ($_GET['azione'] == 3) {
+  $result = getPostParticipantsUsers($_GET['postId'], $mysqli);
 }
 
 header("Content-Type: application/json");
