b1e56eb04aa85407b55c7e13d106283d9750bd1c
S-furi <stefano.furi@studio.unibo.it>
2023-01-24 20:36:24 +0100

    Better indentation

--- website/index.php
+++ website/index.php
@@ -2,9 +2,8 @@
 require_once("bootstrap.php");
 
 $templateParams["Titolo"] = "Brogram - Home";
-
-
 $templateParams["active-home"] = "active";
+
 // inserting the new script, after importing axios
 array_splice($templateParams["js"], 1, 0, "js/timeline.js");
 require("template/base.php");
