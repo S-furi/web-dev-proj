41f9e12680609d942362242bd0c6bed2d1707b1c
S-furi <stefano.furi@studio.unibo.it>
2023-01-24 20:35:14 +0100

    Removed unused php script

--- website/index.php
+++ website/index.php
@@ -3,7 +3,7 @@ require_once("bootstrap.php");
 
 $templateParams["Titolo"] = "Brogram - Home";
 
-// $templateParams["template_name"] = "timeline.php";
+
 $templateParams["active-home"] = "active";
 // inserting the new script, after importing axios
 array_splice($templateParams["js"], 1, 0, "js/timeline.js");
